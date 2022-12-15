d39a92f11c2b27438561d628b2f9b3a657ada7ff
Dan Lynch <pyramation@gmail.com>
2022-12-15 01:47:07 -0800

    clear files for casing issues

--- types/codegen/cosmos/authz/v1beta1/query.lcd.d.ts
+++
@@ -1,9 +0,0 @@
-import { LCDClient } from "@osmonauts/lcd";
-import { QueryGrantsRequest, QueryGrantsResponseSDKType } from "./query";
-export declare class LCDQueryClient {
-    req: LCDClient;
-    constructor({ requestClient }: {
-        requestClient: LCDClient;
-    });
-    grants(params: QueryGrantsRequest): Promise<QueryGrantsResponseSDKType>;
-}
