4451c9fbe24ccf7c9b250f05d8e5408bead223c2
Assaf Morami <assaf.morami@gmail.com>
2024-07-20 20:28:29 +0300

    export a bunch of useful utils
    - coinFromString
    - coinsFromString
    - feeFromGas
    - ibcDenom
    - pubkeyToAddress
    - base64PubkeyToAddress
    - selfDelegatorAddressToValidatorAddress
    - validatorAddressToSelfDelegatorAddress
    - tendermintPubkeyToValconsAddress
    - base64TendermintPubkeyToValconsAddress
    - sleep
    - decToString
    - convertBech32Prefix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "@cosmjs/encoding"; // fromAscii, toAscii, fromBase64, toBase64, fromBech32, normalizeBech32, toBech32, fromHex, toHex, fromRfc3339, toRfc3339, fromUtf8, toUtf8
 export * from "./codegen";
 export * from "./parser";
+export * from "./utils";
