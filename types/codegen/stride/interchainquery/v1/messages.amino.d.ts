d0f986ebe49f701f38676154f0bc9a9759c02323
Assaf Morami <assaf.morami@gmail.com>
2024-09-05 20:40:11 +0300

    untrack types/

--- types/codegen/stride/interchainquery/v1/messages.amino.d.ts
+++
@@ -1,8 +0,0 @@
-import { MsgSubmitQueryResponse } from "./messages";
-export declare const AminoConverter: {
-    "/stride.interchainquery.v1.MsgSubmitQueryResponse": {
-        aminoType: string;
-        toAmino: (message: MsgSubmitQueryResponse) => import("./messages").MsgSubmitQueryResponseAmino;
-        fromAmino: (object: import("./messages").MsgSubmitQueryResponseAmino) => MsgSubmitQueryResponse;
-    };
-};
--- types/parser.d.ts
+++
@@ -1,6 +0,0 @@
-import { Account } from "@cosmjs/stargate";
-import { Any } from "cosmjs-types/google/protobuf/any";
-/**
- * A wrapper for Stargate's default accountParser that adds support for `StridePeriodicVestingAccount`.
- */
-export declare function strideAccountParser(input: Any): Account;
