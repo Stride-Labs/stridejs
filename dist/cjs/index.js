e4141f7c26490e2354acb6808fc0e1b8fa3d3bb6
Assaf Morami <assaf.morami@gmail.com>
2025-02-18 10:57:58 +0200

    pnpm build

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -18,12 +18,17 @@ var __reExport = (target, mod, secondTarget) => (__copyProps(target, mod, "defau
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var src_exports = {};
 __export(src_exports, {
+  DirectSecp256k1HdWallet: () => import_proto_signing.DirectSecp256k1HdWallet,
+  GasPrice: () => import_stargate.GasPrice,
+  LedgerSigner: () => import_ledger_amino.LedgerSigner,
+  Secp256k1HdWallet: () => import_amino.Secp256k1HdWallet,
   fromAscii: () => import_encoding.fromAscii,
   fromBase64: () => import_encoding.fromBase64,
   fromBech32: () => import_encoding.fromBech32,
   fromHex: () => import_encoding.fromHex,
   fromRfc3339: () => import_encoding.fromRfc3339,
   fromUtf8: () => import_encoding.fromUtf8,
+  makeCosmoshubPath: () => import_amino.makeCosmoshubPath,
   normalizeBech32: () => import_encoding.normalizeBech32,
   toAscii: () => import_encoding.toAscii,
   toBase64: () => import_encoding.toBase64,
@@ -33,19 +38,28 @@ __export(src_exports, {
   toUtf8: () => import_encoding.toUtf8
 });
 module.exports = __toCommonJS(src_exports);
+var import_amino = require("@cosmjs/amino");
 var import_encoding = require("@cosmjs/encoding");
+var import_ledger_amino = require("@cosmjs/ledger-amino");
+var import_proto_signing = require("@cosmjs/proto-signing");
+var import_stargate = require("@cosmjs/stargate");
 __reExport(src_exports, require("./codegen"), module.exports);
 __reExport(src_exports, require("./parser"), module.exports);
 __reExport(src_exports, require("./stride_client"), module.exports);
 __reExport(src_exports, require("./utils"), module.exports);
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
+  DirectSecp256k1HdWallet,
+  GasPrice,
+  LedgerSigner,
+  Secp256k1HdWallet,
   fromAscii,
   fromBase64,
   fromBech32,
   fromHex,
   fromRfc3339,
   fromUtf8,
+  makeCosmoshubPath,
   normalizeBech32,
   toAscii,
   toBase64,
--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,3 +1,4 @@
+import { makeCosmoshubPath, Secp256k1HdWallet } from "@cosmjs/amino";
 import {
   fromAscii,
   fromBase64,
@@ -13,17 +14,25 @@ import {
   toRfc3339,
   toUtf8
 } from "@cosmjs/encoding";
+import { LedgerSigner } from "@cosmjs/ledger-amino";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { GasPrice } from "@cosmjs/stargate";
 export * from "./codegen";
 export * from "./parser";
 export * from "./stride_client";
 export * from "./utils";
 export {
+  DirectSecp256k1HdWallet,
+  GasPrice,
+  LedgerSigner,
+  Secp256k1HdWallet,
   fromAscii,
   fromBase64,
   fromBech32,
   fromHex,
   fromRfc3339,
   fromUtf8,
+  makeCosmoshubPath,
   normalizeBech32,
   toAscii,
   toBase64,
