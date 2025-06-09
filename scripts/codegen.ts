012319ec777b983e4e2a427618d290658e41dfe5
Assaf Morami <assaf.morami@gmail.com>
2025-06-09 20:49:16 +0300

    removed amino exceptions
    unnecessary anymore do to using `option (amino.name)`

--- scripts/codegen.ts
+++ scripts/codegen.ts
@@ -29,65 +29,6 @@ telescope({
     },
     aminoEncoding: {
       enabled: true,
-      exceptions: {
-        "/stride.stakeibc.MsgLiquidStake": {
-          aminoType: "stakeibc/LiquidStake",
-        },
-        "/stride.stakeibc.MsgLSMLiquidStake": {
-          aminoType: "stakeibc/LSMLiquidStake",
-        },
-        "/stride.stakeibc.MsgRedeemStake": {
-          aminoType: "stakeibc/RedeemStake",
-        },
-        "/stride.stakeibc.MsgRegisterHostZone": {
-          aminoType: "stakeibc/RegisterHostZone",
-        },
-        "/stride.stakeibc.MsgClaimUndelegatedTokens": {
-          aminoType: "stakeibc/ClaimUndelegatedTokens",
-        },
-        "/stride.stakeibc.MsgRebalanceValidators": {
-          aminoType: "stakeibc/RebalanceValidators",
-        },
-        "/stride.stakeibc.MsgAddValidator": {
-          aminoType: "stakeibc/AddValidator",
-        },
-        "/stride.stakeibc.MsgChangeValidatorWeight": {
-          aminoType: "stakeibc/ChangeValidatorWeight",
-        },
-        "/stride.stakeibc.MsgDeleteValidator": {
-          aminoType: "stakeibc/DeleteValidator",
-        },
-        "/stride.stakeibc.MsgRestoreInterchainAccount": {
-          aminoType: "stakeibc/RestoreInterchainAccount",
-        },
-        "/stride.stakeibc.MsgUpdateValidatorSharesExchRate": {
-          aminoType: "stakeibc/UpdateValidatorSharesExchRate",
-        },
-        "/stride.stakeibc.MsgClearBalance": {
-          aminoType: "still-no-defined",
-        },
-        "/stride.claim.MsgClaimFreeAmount": {
-          aminoType: "claim/ClaimFreeAmount",
-        },
-        "/stride.staketia.MsgLiquidStake": {
-          aminoType: "staketia/MsgLiquidStake",
-        },
-        "/stride.staketia.MsgRedeemStake": {
-          aminoType: "staketia/MsgRedeemStake",
-        },
-        "/stride.stakedym.MsgLiquidStake": {
-          aminoType: "stakedym/MsgLiquidStake",
-        },
-        "/stride.stakedym.MsgRedeemStake": {
-          aminoType: "stakedym/MsgRedeemStake",
-        },
-        "/stride.airdrop.MsgClaimDaily": {
-          aminoType: "airdrop/MsgClaimDaily",
-        },
-        "/stride.airdrop.MsgClaimEarly": {
-          aminoType: "airdrop/MsgClaimEarly",
-        },
-      },
     },
     lcdClients: {
       enabled: false,
